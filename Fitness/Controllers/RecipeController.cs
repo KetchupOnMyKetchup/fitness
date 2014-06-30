605c4060234f2a05839fd52fb9590cad8cf9e6e6
Philip Tenn <ptenn@users.noreply.github.com>
2014-06-29 23:23:44 -0400

    Work on Recipe Create View.  Add TinyMCE via CDN

--- Fitness/Controllers/RecipeController.cs
+++ Fitness/Controllers/RecipeController.cs
@@ -55,23 +55,6 @@ namespace Fitness.Controllers
         }
 
 
-        // GET: /Recipe/Details/5
-//        public ActionResult Details(int? id)
-//        {
-//            if (id == null)
-//            {
-//                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-//            }
-//            Recipe recipe = db.Recipes.Find(id);
-//            if (recipe == null)
-//            {
-//               return HttpNotFound();
-//            }
-//            return View(recipe);
-//        }
-
- 
-
         // GET: /Recipe/Create
         public ActionResult Create()
         {
